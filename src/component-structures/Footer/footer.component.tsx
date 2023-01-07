a1e0fc2b58b57af1964809029138123c7f04e7bb
Alex  Salgado <alexgsalgado@gmail.com>
2023-01-07 10:58:55 -0300

    feat(style): added rem function to sass and added wrapper and grid components to structure component

--- src/component-structures/Footer/footer.component.tsx
+++ src/component-structures/Footer/footer.component.tsx
@@ -3,12 +3,14 @@ import { useNavigate } from 'react-router-dom';
 import { version } from '../../../package.json';
 // context
 // components
+import WrapperBlock from '../WrapperBlock/wrapper-block.component';
 // imports
 import CategoriesApi from '../../services/categories.services';
 import Careers from '../../_mock_/career-list';
 // styles and images
 import styles from './footer.module.scss';
 import Logo from '../../components/Logo/logo.component';
+import GridBlock from '../GridBlock/grid-block.component';
 
 const Footer = () => {
   const [career, setCareers] = useState<Array<{ id: number; title: string }>>(
@@ -22,46 +24,48 @@ const Footer = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={`${styles.footer_wrapper} ${styles.footer_grid}`}>
-        <section className={styles.footer_section}>
-          <Logo logoId='footer_logo' type='dark' linkTo='/' />
-          <p className={styles.footer_section}>
-            This project is a perfect version of a &quot;useless&quot; store that makes no sense and
-            shows products that you definitely don&apos;t want and I bet your clicked anyway because
-            you saw the photo and not the title.
-          </p>
-          <p>
-            Version: <strong>{version}</strong>
-          </p>
-        </section>
+      <WrapperBlock>
+        <GridBlock xs={2} md={3}>
+          <section className={styles.footer_section}>
+            <Logo logoId='footer_logo' type='dark' linkTo='/' />
+            <p className={styles.footer_section}>
+              This project is a perfect version of a &quot;useless&quot; store that makes no sense
+              and shows products that you definitely don&apos;t want and I bet your clicked anyway
+              because you saw the photo and not the title.
+            </p>
+            <p>
+              Version: <strong>{version}</strong>
+            </p>
+          </section>
 
-        {/* Category list */}
-        <section className={styles.footer_section}>
-          <h4>Categories</h4>
-          <nav className={styles.footer_list} aria-label='Category list'>
-            {errorCategory
-              ? 'No category to display'
-              : categoryList?.data.map((category) => (
-                  <a key={category.id} aria-label='Category item'>
-                    - {category.name}
-                  </a>
-                ))}
-          </nav>
-        </section>
+          {/* Category list */}
+          <section className={styles.footer_section}>
+            <h4>Categories</h4>
+            <nav className={styles.footer_list} aria-label='Category list'>
+              {errorCategory
+                ? 'No category to display'
+                : categoryList?.data.map((category) => (
+                    <a key={category.id} aria-label='Category item'>
+                      - {category.name}
+                    </a>
+                  ))}
+            </nav>
+          </section>
 
-        {/* Career list */}
-        <section className={styles.footer_section}>
-          <h4>Careers</h4>
-          <nav className={styles.footer_list} aria-label='Career list'>
-            {/* Career title and link */}
-            {career?.map((item) => (
-              <a key={item.id} aria-label='Career item'>
-                - {item.title}
-              </a>
-            ))}
-          </nav>
-        </section>
-      </div>
+          {/* Career list */}
+          <section className={styles.footer_section}>
+            <h4>Careers</h4>
+            <nav className={styles.footer_list} aria-label='Career list'>
+              {/* Career title and link */}
+              {career?.map((item) => (
+                <a key={item.id} aria-label='Career item'>
+                  - {item.title}
+                </a>
+              ))}
+            </nav>
+          </section>
+        </GridBlock>
+      </WrapperBlock>
     </footer>
   );
 };
